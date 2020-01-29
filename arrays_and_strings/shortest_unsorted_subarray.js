a74599307bc73212f32225f652fe450825cd7065
JP Sio <sio.jeanpaul@gmail.com>
2020-01-28 23:40:58 -0800

    Adds test cases

---
+++ arrays_and_strings/shortest_unsorted_subarray.js
@@ -0,0 +1,14 @@
+/**
+ * Given an array of integers, find the shortest subarray,
+ * that if sorted, results in the entire array being sorted.
+ *
+ * Return the start and end indices of the subarray as a pair.
+ *
+ * Given: [1, 2, 3, 5, 4, 10, 9, 12]
+ * Result: [3, 6] since if we sort [5, 4, 10, 9], the
+ * entire array would be sorted
+ */
+
+export function shortestUnsortedSubarray(arr) {
+  return arr;
+}
