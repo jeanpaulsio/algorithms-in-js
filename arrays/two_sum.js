a83347849bd7eefc0273cdd51a4d94d60a00a4fe
JP Sio <hello@jpsio.me>
2020-09-07 01:35:54 -0700

    Two sum

--- arrays/two_sum.js
+++ arrays/two_sum.js
@@ -1,3 +1,32 @@
 export const twoSum = (arr, x) => {
-  return x;
+  if (arr === null || x === null) {
+    throw new Error("Input must not be null.");
+  }
+
+  if (arr.length === 1) {
+    throw new Error("Array must have at least 2 items.");
+  }
+
+  const result = [-1, -1];
+
+  let i = 0;
+  let j = arr.length - 1;
+
+  while (i < j) {
+    const currentSum = arr[i] + arr[j];
+
+    if (currentSum === x) {
+      result[0] = i;
+      result[1] = j;
+      break;
+    }
+
+    if (currentSum > x) {
+      j -= 1;
+    } else if (currentSum < x) {
+      i += 1;
+    }
+  }
+
+  return result;
 };
