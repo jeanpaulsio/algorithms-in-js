a56cae37794e8288611882eb082af4f9a0e6b094
JP Sio <hello@jpsio.me>
2020-11-15 23:07:01 -0800

    Add dutch national flag

---
+++ arrays/__tests__/dutch_national_flag.test.js
@@ -0,0 +1,13 @@
+import { dutchNationalFlag } from "@arrays/dutch_national_flag";
+
+test("sorts 0s, 1s, and 2s", () => {
+  const actual = dutchNationalFlag([0, 1, 2, 0, 1, 2, 0, 1, 2]);
+  const expected = [0, 0, 0, 1, 1, 1, 2, 2, 2];
+  expect(actual).toEqual(expected);
+});
+
+test("already sorted", () => {
+  const actual = dutchNationalFlag([0, 1, 2]);
+  const expected = [0, 1, 2];
+  expect(actual).toEqual(expected);
+});
---
+++ arrays/dutch_national_flag.js
@@ -0,0 +1,26 @@
+const swap = (arr, i, j) => {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+};
+
+export const dutchNationalFlag = (arr) => {
+  let low = 0;
+  let mid = 0;
+  let high = arr.length - 1;
+
+  while (mid <= high) {
+    if (arr[mid] === 0) {
+      swap(arr, low, mid);
+      low++;
+      mid++;
+    } else if (arr[mid] === 1) {
+      mid++;
+    } else if (arr[mid] === 2) {
+      swap(arr, high, mid);
+      high--;
+    }
+  }
+
+  return arr;
+};
