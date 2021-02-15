700b1362b68f34074e4798493b85a826c962a205
JP Sio <hello@jpsio.me>
2021-02-15 01:06:04 -0800

    wrap tests in describe blocks

--- arrays/__tests__/dutch_national_flag.test.js
+++ arrays/__tests__/dutch_national_flag.test.js
@@ -1,13 +1,15 @@
 import { dutchNationalFlag } from "@arrays/dutch_national_flag";
 
-test("sorts 0s, 1s, and 2s", () => {
-  const actual = dutchNationalFlag([0, 1, 2, 0, 1, 2, 0, 1, 2]);
-  const expected = [0, 0, 0, 1, 1, 1, 2, 2, 2];
-  expect(actual).toEqual(expected);
-});
+describe("dutchNationalFlag", () => {
+  test("sorts 0s, 1s, and 2s", () => {
+    const actual = dutchNationalFlag([0, 1, 2, 0, 1, 2, 0, 1, 2]);
+    const expected = [0, 0, 0, 1, 1, 1, 2, 2, 2];
+    expect(actual).toEqual(expected);
+  });
 
-test("already sorted", () => {
-  const actual = dutchNationalFlag([0, 1, 2]);
-  const expected = [0, 1, 2];
-  expect(actual).toEqual(expected);
+  test("already sorted", () => {
+    const actual = dutchNationalFlag([0, 1, 2]);
+    const expected = [0, 1, 2];
+    expect(actual).toEqual(expected);
+  });
 });
